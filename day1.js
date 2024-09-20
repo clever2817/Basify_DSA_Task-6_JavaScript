4237eef8bba6ddabbeea4018ad3cabd9521104c4
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-09-20 19:12:37 +0600

    Problem (1 - 5)
    Problem (1 - 5)

---
+++ day1.js
@@ -0,0 +1,72 @@
+// variable
+
+//Problem-1 (Number Data Type)
+let CaptainBinary = 100;
+
+console.log(CaptainBinary);
+
+//Problem-2 (String Data Type)
+let MyName = "Menhajul Abedin Abir";
+
+console.log(MyName);
+
+//Problem-3 (Summation)
+let num1 = 1000;
+let num2 = 1000;
+
+let sum1 = num1 + num2;
+
+console.log(sum1);
+
+// Problem-4 (Summation, Subtraction, Multiplication, Division)
+let x = 1000;
+let y = 500;
+
+let sum2 = x + y;
+let sub = x - y;
+let mul = x * y;
+let div = x / y;
+
+console.log(sum2);
+console.log(sub);
+console.log(mul);
+console.log(div);
+
+// problem-5 (Data Types: Primitive & Non-primitive)
+
+//Primitive Data Types
+
+// String
+let myString = "Hello World";
+
+// Number
+let myNumber = 100;
+
+// Boolean
+let myBoolean = true;
+
+// Undefined
+let myUndefined = undefined;
+
+// Null
+let myNull = null;
+
+// BigInt
+let myBigInt = BigInt("12345");
+
+// Symbol
+let mySymbol = Symbol("Hello");
+
+// Non-Primitive Data Types
+
+// Array
+let myArray = [10, 20, 30];
+
+//Object
+let myObject = {
+  fullName: "Menhajul Abedin Abir",
+  age: 24,
+  designation: "Web Developer",
+  isStudent: true,
+  institution: "Basify",
+};
